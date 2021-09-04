5696b20fd4e1bb04abe27e64c033839ffc9e40aa
namekosiru <rinjyou0031@toki.waseda.jp>
2021-09-04 20:12:27 +0900

    レスポンシブ機能の修正

--- src/component/atoms/Feature.jsx
+++ src/component/atoms/Feature.jsx
@@ -2,14 +2,14 @@ import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 export const Feature = ({ title, desc, date }) => {
   return (
-    <Box my={2} w={{ base: 400, md: 800 }} borderBottom="double">
+    <Box my={2} w={{ base: 250, md: 800 }} borderBottom="double">
       <Flex pb={2}>
-        <Heading fontSize="xl" mr={10}>
+        <Heading fontSize="xl" mr={10} fontSize={{ base: "16px", md: "22px" }}>
           {title}
         </Heading>
-        <Text>{date}</Text>
+        <Text fontSize={{ base: "15px", md: "18px" }}>{date}</Text>
       </Flex>
-      <Text>{desc}</Text>
+      <Text fontSize={{ base: "15px", md: "18px" }}>{desc}</Text>
     </Box>
   );
 };
